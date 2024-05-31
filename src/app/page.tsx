319f61fb94c92151ff5934a9169ba788954d793f
habib33-3 <habib.swi16@gmail.com>
2024-05-31 18:43:56 +0600

    search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import ProductCards from "@/components/ProductsCards/ProductCards";
+import SearchBox from "@/components/ProductsCards/SearchBox/SearchBox";
 import { getAllProducts } from "@/utils/api";
 
-const HomePage = async () => {
+const HomePage = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    title?: string;
+  };
+}) => {
   const products = await getAllProducts();
 
+  const title = searchParams?.title || "";
+
   return (
-    <>
-      <ProductCards products={products} />
-    </>
+    <div>
+      <SearchBox />
+      <ProductCards
+        products={products}
+        searchQuery={title}
+      />
+    </div>
   );
 };
 
--- src/components/ProductsCards/ProductCards.tsx
+++ src/components/ProductsCards/ProductCards.tsx
@@ -1,10 +1,24 @@
+"use client";
+
+import { useMemo } from "react";
 import ProductCard from "./ProductCard/ProductCard";
 
-const ProductCards = ({ products }: { products: ProductType[] }) => {
+interface ProductCardsProps {
+  products: ProductType[];
+  searchQuery: string;
+}
+
+const ProductCards = ({ products, searchQuery }: ProductCardsProps) => {
+  const filteredProducts = useMemo(() => {
+    return products.filter((product) =>
+      product.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [products, searchQuery]);
+
   return (
     <div>
       <div className="flex flex-wrap items-center justify-center gap-10">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <ProductCard
             product={product}
             key={product.id}
